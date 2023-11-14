652a37b07f3b894e6562cbcf7fa704ffab8183d1
1r2o3b4e5l <robelamsalu44@gmail.com>
2023-11-14 10:57:10 -0800

    add prt_pp.c

---
+++ prt_pp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * prt_pp - prints the % sign when used in the printf funciton.
+ *
+ * Return: 1
+*/
+int prt_pp(void)
+{
+	_putchar('%'); /* print the % and return 1 */
+
+	return (1);
+}
